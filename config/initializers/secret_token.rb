1313634c8345f4cf500b54754dd31dcad9b7666f
Alan Carl Brown <syracal@gmail.com>
2013-08-27 17:44:26 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Traballous::Application.config.secret_key_base = 'a78469059141b0f95e11a965d93ad79ce572deeca971a53b4e664a495563a8b47a9d329ea7525365082d3dcac966bdb77e891f8cfbc826961cc60112cf1ff97f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Traballous::Application.config.session_store :cookie_store, key: '_traballous_session'
